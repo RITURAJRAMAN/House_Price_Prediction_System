9dd62b629b4e7c8f7f8419744410f58fec245959
RITURAJ RAMAN <riturajraman12345@gmail.com>
2023-03-19 17:05:41 +0530

    Commit

--- app.py
+++ app.py
@@ -16,9 +16,15 @@ def home():
 def predict():
     input_data = [float(x) for x in request.form.values()]
     variables_data = [np.array(input_data)]
-
-    features_name = ['mean radius', 'worst fractal dimension']
-
+    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+                     'mean smoothness', 'mean compactness', 'mean concavity',
+                     'mean concave points', 'mean symmetry', 'mean fractal dimension',
+                     'radius error', 'texture error', 'perimeter error', 'area error',
+                     'smoothness error', 'compactness error', 'concavity error',
+                     'concave points error', 'symmetry error', 'fractal dimension error',
+                     'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+                     'worst smoothness', 'worst compactness', 'worst concavity',
+                     'worst concave points', 'worst symmetry', 'worst fractal dimension']
     df = pd.DataFrame(variables_data, columns=features_name)
     house_price = model.predict(df)
     return render_template('index.html', prediction_text='The Estimated Price for the House is {}'.format(house_price))
