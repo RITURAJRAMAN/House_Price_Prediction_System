b2afd3bf78cc573bb17dd9b6a7ce0aaf3828fca8
RITURAJ RAMAN <riturajraman12345@gmail.com>
2023-03-19 17:43:58 +0530

    Commit

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def home():
 def predict():
     input_data = [float(x) for x in request.form.values()]
     variables_data = [np.array(input_data)]
+
     features_name = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street', 'Alley', 'LotShape', 'LandContour',
                      'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
                      'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'RoofStyle', 'RoofMatl',
@@ -28,6 +29,7 @@ def predict():
                      'GarageQual', 'GarageCond', 'PavedDrive', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch',
                      '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature', 'MiscVal', 'MoSold',
                      'YrSold', 'SaleType', 'SaleCondition', 'SalePrice']
+
     df = pd.DataFrame(variables_data, columns=features_name)
     house_price = model.predict(df)
     return render_template('index.html', prediction_text='The Estimated Price for the House is {}'.format(house_price))
